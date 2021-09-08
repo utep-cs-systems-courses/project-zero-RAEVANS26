493137a4e0b88b1be62c62f0fdb261d5be2011ff
Robert <raevans@miners.utep.edu>
2021-09-08 11:42:01 -0600

    added print_arrow method

--- draw.h
+++ draw.h
@@ -1,6 +1,7 @@
 #ifndef DRAW_INCLUDED
 #define DRAW_INCLUDED
 
+void print_arrow(int startCol, int size);
 void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
 void print_char_11x16(char c);
