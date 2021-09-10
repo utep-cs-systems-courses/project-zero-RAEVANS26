c19d49a7e2753ac9133c8a664431c0f310cab90d
Robert <raevans@miners.utep.edu>
2021-09-10 01:29:31 -0600

    added print_char_5x7 and print_char_8x12

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,29 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void print_char_5x7(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 5; row++) {
+    unsigned char rowBits = font_5x7[c][row];
+    for (char col = 0; col < 7; col++) {
+      unsigned char colMask = 1 << (6-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 12; row++) {
+    unsigned char rowBits = font_8x12[c][row];
+    for (char col = 0; col < 8; col++) {
+      unsigned char colMask = 1 << (7-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars(11x16) = c, Chars(11x16) = d, \nChars(11x16) = e, Arrow = a) or 'q' to quit.\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -25,9 +25,19 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected chars(11x16):");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+        print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected chars(5x7):");
+      for (char c = 'A'; c < 'D'; c++)
+        print_char_5x7(c);
+      break;
+    case 'e':
+      puts("You selected chars(8x12):");
+      for (char c = 'A'; c < 'D'; c++)
+        print_char_8x12(c);
       break;
     case 'a':
       puts("You selected arrow:");
