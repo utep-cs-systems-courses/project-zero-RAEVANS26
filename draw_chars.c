621949abf4dc847a84f6ae9a9ebb923645b2a18c
Robert <raevans@miners.utep.edu>
2021-09-13 12:30:51 -0600

    Changed print_char_8x12() and fixed typo in uimain.

--- draw_chars.c
+++ draw_chars.c
@@ -16,26 +16,26 @@ void print_char_11x16(char c)
 
 void print_char_5x7(char c)
 {
-  c -= 0x20;
-  for (char row = 0; row < 5; row++) {
-    unsigned char rowBits = font_5x7[c][row];
-    for (char col = 0; col < 7; col++) {
-      unsigned char colMask = 1 << (6-col); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+    c -= 0x20;
+    for (char row = 0; row < 5; row++) {
+        unsigned char rowBits = font_5x7[c][row];
+        for (char col = 0; col < 7; col++) {
+            unsigned char colMask = 1 << (6-col); /* mask to select bit associated with bit */
+            putchar( (rowBits & colMask) ? '*' : ' ');
+        }
+        putchar('\n');
     }
-    putchar('\n');
-  }
 }
 
 void print_char_8x12(char c)
 {
-  c -= 0x20;
-  for (char row = 0; row < 12; row++) {
-    unsigned char rowBits = font_8x12[c][row];
-    for (char col = 0; col < 8; col++) {
-      unsigned char colMask = 1 << (7-col); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+    c -= 0x20;
+    for (char col = 0; col < 8; col++){
+        for (char row = 11; row >=0 ; row--) {
+            unsigned char rowBits = font_8x12[c][row];
+            unsigned char colMask = 1 << (7-col); /* mask to select bit associated with bit */
+            putchar( (rowBits & colMask) ? '*' : ' ');
+        }
+        putchar('\n');
     }
-    putchar('\n');
-  }
 }
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars(11x16) = c, Chars(11x16) = d, \nChars(11x16) = e, Arrow = a) or 'q' to quit.\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars(11x16) = c, Chars(5x7) = d, \nChars(8x12) = e, Arrow = a) or 'q' to quit.\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
